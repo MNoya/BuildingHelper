1c1484921efdaafbb6d6fc1f4892c02f8c4b70d0
MNoya <martinnoya@gmail.com>
2016-01-05 22:56:59 -0300

    Clean clicks and unit_selection
    - Transparent Activate through debug calls
    - Added ability_building
    - Updated content folder

---
+++ panorama/scripts/clicks.js
@@ -0,0 +1,90 @@
+"use strict"
+
+var attackTable = CustomNetTables.GetAllTableValues( "attacks_enabled" )
+
+function GetMouseTarget()
+{
+    var mouseEntities = GameUI.FindScreenEntities( GameUI.GetCursorPosition() )
+    var localHeroIndex = Players.GetPlayerHeroEntityIndex( Players.GetLocalPlayer() )
+
+    for ( var e of mouseEntities )
+    {
+        if ( !e.accurateCollision )
+            continue
+        return e.entityIndex
+    }
+
+    for ( var e of mouseEntities )
+    {
+        return e.entityIndex
+    }
+
+    return 0
+}
+
+// Handle Right Button events
+function OnRightButtonPressed()
+{
+    $.Msg("OnRightButtonPressed")
+
+    var iPlayerID = Players.GetLocalPlayer()
+    var selectedEntities = Players.GetSelectedEntities( iPlayerID )
+    var mainSelected = Players.GetLocalPlayerPortraitUnit() 
+    var targetIndex = GetMouseTarget()
+    var pressedShift = GameUI.IsShiftDown()
+
+    // Builder Right Click
+    if ( IsBuilder( mainSelected ) )
+    {
+        // Cancel BH
+        if (!pressedShift) SendCancelCommand()
+    }
+
+    return false
+}
+
+// Handle Left Button events
+function OnLeftButtonPressed() {
+    $.Msg("OnLeftButtonPressed")
+
+    return false
+}
+
+function IsBuilder(entIndex) {
+    var tableValue = CustomNetTables.GetTableValue( "builders", entIndex.toString())
+    return (tableValue !== undefined) && (tableValue.IsBuilder == 1)
+}
+
+(function () {
+    CustomNetTables.SubscribeNetTableListener( "attacks_enabled", OnAttacksEnabledChanged );
+})();
+
+// Main mouse event callback
+GameUI.SetMouseCallback( function( eventName, arg ) {
+    var CONSUME_EVENT = true
+    var CONTINUE_PROCESSING_EVENT = false
+    var LEFT_CLICK = (arg === 0)
+    var RIGHT_CLICK = (arg === 1)
+
+    if ( GameUI.GetClickBehaviors() !== CLICK_BEHAVIORS.DOTA_CLICK_BEHAVIOR_NONE )
+        return CONTINUE_PROCESSING_EVENT
+
+    var mainSelected = Players.GetLocalPlayerPortraitUnit()
+
+    if ( eventName === "pressed" || eventName === "doublepressed")
+    {
+        // Builder Clicks
+        if (IsBuilder(mainSelected))
+            if (LEFT_CLICK) 
+                return (state == "active") ? SendBuildCommand() : OnLeftButtonPressed()
+            else if (RIGHT_CLICK) 
+                return OnRightButtonPressed()
+
+        if (LEFT_CLICK) 
+            return OnLeftButtonPressed()
+        else if (RIGHT_CLICK) 
+            return OnRightButtonPressed() 
+        
+    }
+    return CONTINUE_PROCESSING_EVENT
+} )
