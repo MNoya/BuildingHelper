69224c22df0456c5e24c8be8755c5d4773182741
MNoya <martinnoya@gmail.com>
2016-08-19 00:30:39 -0300

    Improved various modifiers

--- scripts/vscripts/libraries/modifiers/modifier_builder_hidden.lua
+++ scripts/vscripts/libraries/modifiers/modifier_builder_hidden.lua
@@ -11,26 +11,17 @@ if IsServer() then
             [MODIFIER_STATE_DISARMED] = true,
             [MODIFIER_STATE_NOT_ON_MINIMAP] = true,
             [MODIFIER_STATE_NO_HEALTH_BAR] = true,
+            [MODIFIER_STATE_BLIND] = true,
         }
 
         return state
-    end
-
-    function modifier_builder_hidden:OnCreated( params )    
-        local unit = self:GetParent()
-        unit.originalDayVision = unit:GetDayTimeVisionRange()
-        unit.originalNightVision = unit:GetDayTimeVisionRange()
-        unit:SetDayTimeVisionRange(0)
-        unit:SetNightTimeVisionRange(0)
-    end
-
-    function modifier_builder_hidden:OnDestroy( params )
-        local unit = self:GetParent()
-        unit:SetDayTimeVisionRange(unit.originalDayVision)
-        unit:SetNightTimeVisionRange(unit.originalNightVision)
     end
 end
 
 function modifier_builder_hidden:IsHidden()
     return true
+end
+
+function modifier_builder_hidden:IsPurgable()
+    return false
 end
